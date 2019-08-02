3c1bc59d3ae9df07c2a4f6042c5ec78b0436a283
Gacki, Robert (EXTERN: CGI) <extern.robert.gacki@volkswagen.de>
2019-08-02 10:20:25 +0200

    Filter must be ordered

--- src/main/java/org/contenttrace/springframework/cloud/gateway/cache/CreateCacheEntryFilter.java
+++ src/main/java/org/contenttrace/springframework/cloud/gateway/cache/CreateCacheEntryFilter.java
@@ -6,12 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.WebClientWriteResponseFilter;
+import org.springframework.core.Ordered;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import static java.util.Objects.requireNonNull;
 
-public class CreateCacheEntryFilter implements GatewayFilter {
+public class CreateCacheEntryFilter implements GatewayFilter, Ordered {
 
   private static final Logger LOG = LoggerFactory.getLogger(CreateCacheEntryFilter.class);
 
@@ -49,5 +51,10 @@ public class CreateCacheEntryFilter implements GatewayFilter {
     }
 
     return chain.filter(store.write(exchange));
+  }
+
+  @Override
+  public int getOrder() {
+    return WebClientWriteResponseFilter.WRITE_RESPONSE_FILTER_ORDER - 1;
   }
 }
