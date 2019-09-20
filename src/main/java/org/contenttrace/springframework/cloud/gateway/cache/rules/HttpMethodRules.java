92f91c87796061e2f08e6a5c8fd464cfcb08ef64
Robert Gacki <robert.gacki@contenttrace.org>
2019-09-20 18:40:26 +0200

    Minor fix

--- src/main/java/org/contenttrace/springframework/cloud/gateway/cache/rules/HttpMethodRules.java
+++ src/main/java/org/contenttrace/springframework/cloud/gateway/cache/rules/HttpMethodRules.java
@@ -5,10 +5,10 @@ import org.springframework.http.HttpMethod;
 public final class HttpMethodRules {
 
   private static final Rule IS_GET_REQUEST = exchange -> exchange.getRequest().getMethod() == HttpMethod.GET;
-  private static final Rule IS_HEAD_REQUEST = exchange -> exchange.getRequest().getMethod() == HttpMethod.GET;
+  private static final Rule IS_HEAD_REQUEST = exchange -> exchange.getRequest().getMethod() == HttpMethod.HEAD;
 
   public static Rule isGetRequest() {
-    return IS_HEAD_REQUEST;
+    return IS_GET_REQUEST;
   }
 
   public static Rule isHeadRequest() {
